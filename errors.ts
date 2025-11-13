bb4acf0a94fac419632182d74bf0bfcdc824b3f1
Nikita Minyuk <nikita.minyuk.w@gmail.com>
2025-11-13 12:11:42 +0200

    Update errors.ts

--- errors.ts
+++ errors.ts
@@ -1,5 +1,5 @@
 export type Result<T, E> = { ok: true; value: T } | { ok: false; error: E };
-export type AppError<ErrorName extends string, Extra = {}> = Error & Extra;
+export type AppError<ErrorName extends string, Extra = {}> = Error & Extra & { name: ErrorName };
 export const ok = <T>(value: T): Result<T, never> => ({ ok: true, value });
 export const err = <E>(error: E): Result<never, E> => ({ ok: false, error });
 
